d8a5f2450d632f715c097fc510bd67afe9e421e4
Ramkrushna Pathade <rpathade@fortinet.com>
2025-01-07 15:04:41 +0530

    Fixed minor issues.

--- fortinet-fortirecon-aci/aci_operations/iocs.py
+++ fortinet-fortirecon-aci/aci_operations/iocs.py
@@ -15,9 +15,5 @@ def get_iocs(config: dict, params: dict) -> dict:
         params["report_id"] = str(params.get('report_id')).strip('[]')
     endpoint = "/aci/{org_id}/iocs"
 
-    if params.get("start_date"):
-        params["start_date"] = MK.handle_date(params.get("start_date"))
-    if params.get("end_date"):
-        params["end_date"] = MK.handle_date(params.get("end_date"))
     response = MK.make_request(endpoint=endpoint, method="GET", params=params)
     return response
