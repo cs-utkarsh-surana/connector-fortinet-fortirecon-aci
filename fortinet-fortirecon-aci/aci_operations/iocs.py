1e78f42b508a1db2c55c6b699f4285eb2332f883
usurana <usurana@fortinet.com>
2025-02-03 14:24:58 +0530

    Updated the DI playbook and Added Get all Records param in get iocs action

--- fortinet-fortirecon-aci/aci_operations/iocs.py
+++ fortinet-fortirecon-aci/aci_operations/iocs.py
@@ -10,14 +10,24 @@ from ..make_rest_api_call import MakeRestApiCall
 
 def get_iocs(config: dict, params: dict) -> dict:
     MK = MakeRestApiCall(config=config)
-
+    get_all_records = params.pop('get_all_records', None)
     if params.get('report_id'):
         params["report_id"] = str(params.get('report_id')).strip('[]')
     endpoint = "/aci/{org_id}/iocs"
-
     if params.get("start_date"):
         params["start_date"] = MK.handle_date(params.get("start_date"))
     if params.get("end_date"):
         params["end_date"] = MK.handle_date(params.get("end_date"))
+    if get_all_records:
+        params.pop('page', "")
+        params['size'] = 500
+        response = MK.make_request(endpoint=endpoint, method="GET", params=params)
+        iocs = response.get('hits', [])
+        while response.get('hits'):
+            params['page'] = int(params.get('page', 1)) + 1
+            response = MK.make_request(endpoint=endpoint, method="GET", params=params)
+            iocs.extend(response.get('hits', []))
+        response['hits'] = iocs
+        return response
     response = MK.make_request(endpoint=endpoint, method="GET", params=params)
     return response
